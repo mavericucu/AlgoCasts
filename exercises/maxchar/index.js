3fe6a153a332842d2bedc6b5de3b6ba73430cb8f
Ismael Jaouhar <ismael.jaouhar@gmail.com>
2019-03-18 18:31:27 +0100

    Done 4

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
-
+function maxChar(str) {
+  var contador = 0;
+  var totalcontador = 0;
+  var cha = '';
+  var arr = str.split('');
+  for (var i = 0; i<arr.length; i++) {
+    for (var j = 0; j<arr.length; j++) {
+      if (arr[i] = arr[j]){
+        contador++;
+      }
+      if (contador > totalcontador) {
+        totalcontador = contador;
+        cha = arr[i];
+      }
+    }
+  }
+  return cha;
+}
+  
 module.exports = maxChar;
