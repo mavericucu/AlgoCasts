43c22b57cabdb85d7ab5717a1738c2935ea31151
Ismael Jaouhar <ismael.jaouhar@gmail.com>
2019-04-01 15:48:32 +0200

    Vowels done

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,18 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let counter = 0;
+  let arr = [];
+  arr = str.toLowerCase().split("");
+  console.log(arr);
+  arr.forEach(element => {
+    if (element==='a' || element==='e' || element==='i' || element==='o' || element==='u'){
+      counter++;
+    } else {
 
+    }
+  });
+  return counter;
+}
 module.exports = vowels;
