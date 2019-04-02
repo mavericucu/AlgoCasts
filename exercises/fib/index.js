d0a5602ea9c482fd6900618a006dd14eff15ef46
Ismael Jaouhar <ismael.jaouhar@gmail.com>
2019-04-02 13:40:52 +0200

    Fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,33 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
 
 module.exports = fib;
+
+
+
+
+// function fib(n) {
+//   const arr = [];
+//   let value = 0;
+//   for (let i = 0; i <= n; i++) {
+//     if (i === 0) {
+//       arr.push(0);
+//     } else if (i === 1) {
+//       arr.push(1);
+//     } else {
+//       value = arr[i - 1] + arr[i - 2];
+//       arr.push(value);
+//     }
+
+//   }
+//   return arr[n];
+// }
+
+// module.exports = fib;
